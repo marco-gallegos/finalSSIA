be78cb8c7baa928b8d311d375e39d881115e5579
Marco A. Gallegos <ma_galeza@hotmai.com>
2019-05-02 22:34:03 -0500

    update : foco
    se crea funcion que hace plot sobre el contenedor de un foco con forma
    piramidal

--- 1.py
+++ 1.py
@@ -20,7 +20,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
 
-xl = yl = zl = 30
+xl = yl = zl = 20
+# contenedor a partir de los limites
 contenedor = [ [[0 for p1 in range(zl)] for p2 in range(yl)] for p3 in range(xl)]
 contenedor = np.array(contenedor)
 
@@ -36,12 +37,46 @@ def printBox(box, ax , xl, yl ,zl):
                 if box[x][y][z] == 1:
                     ax.scatter3D([x],[y],[z])
 
-def Luminaria(Object):
-    def __init__(self, origen, aumento_z, limite_z):
-        self.origen = origen
+class Bulbo:
+    def __init__(self, origen_x, origen_y, aumento_z, limite_x, limite_y, limite_z):
+        self.origen_x = origen_x
+        self.origen_y = origen_y
         self.aumento_z = aumento_z
+        self.limite_x = limite_x
+        self.limite_y = limite_y
         self.limite_z = limite_z
+    
+    def render(self,box):
+        """
+        Representar el bulbo e el box
+        @param np.array[x][y][z] solos los dos 
+        """
+        newBox = box
+        for i in range(self.limite_z):
+            for x in range(i):
+                for y in range(i):
+                    if self.origen_y + y <= self.limite_y:
+                        newBox[self.origen_x][self.origen_y + y][i] = 1
+                    if self.origen_y - y >= 0:
+                        newBox[self.origen_x][self.origen_y - y][i] = 1
+                    if self.origen_x + x <= self.limite_x:
+                        newBox[self.origen_x + x][self.origen_y][i] = 1
+                    if self.origen_x + x >= 0:
+                        newBox[self.origen_x - x][self.origen_y][i] = 1
+                    if self.origen_x + x <= self.limite_x and self.origen_y + y <= self.limite_y:
+                        newBox[self.origen_x + x][self.origen_y + y ][i] = 1
+                    if self.origen_x + x <= self.limite_x and self.origen_y - y >= 0:
+                        newBox[self.origen_x + x][self.origen_y - y ][i] = 1
+                    if self.origen_x - x >= 0 and self.origen_y + y <= self.limite_y:
+                        newBox[self.origen_x - x][self.origen_y + y ][i] = 1
+                    if self.origen_x - x >= 0 and self.origen_y - y >= 0:
+                        newBox[self.origen_x - x][self.origen_y - y ][i] = 1
+
+        return newBox
         
+# declaracion de un bulbo
+mi_bulbo = Bulbo(origen_x=18,origen_y=18,aumento_z=1,limite_x=19,limite_y=19,limite_z=10)
+#fin de declaracion de un bulbo
 
     
 fig = plt.figure()
@@ -49,6 +84,9 @@ ax = fig.add_subplot(111, projection="3d")
 ax.set_xlabel("Ancho")
 ax.set_ylabel("Largo")
 ax.set_zlabel("Alto")
+
+#plot sobre nuestro contenedor
+contenedor = mi_bulbo.render(contenedor)
 
 printBox(contenedor, ax, xl, yl, zl)
 # ax.scatter3D(x,y,z)
